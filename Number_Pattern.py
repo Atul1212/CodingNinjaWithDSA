5f528a74de3fa4494ebf9d84dfbd0fab96b0b844
Atul1212 <41967153+Atul1212@users.noreply.github.com>
2022-06-25 23:46:29 +0530

    Create Number_Pattern.py

---
+++ Number_Pattern.py
@@ -0,0 +1,28 @@
+'''
+Number Pattern
+Send Feedback
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1234
+123
+12
+1
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Sample Input :
+5
+Sample Output :
+12345
+1234
+123
+12
+1
+'''
+
+n = int(input())
+for i in range(n+1,1,-1):
+    for j in range(1,i):
+        print(j,end='')
+    print()
